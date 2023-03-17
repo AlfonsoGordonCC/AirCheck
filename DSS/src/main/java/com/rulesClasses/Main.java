5c20aa6aac753add8e82fb9a21ce084d9ff4f73c
pablo <p.rodriguez103@usp.ceu.es>
2023-03-17 17:44:14 +0100

    Symptoms and diseases start

--- DSS/src/main/java/com/rulesClasses/Main.java
+++ DSS/src/main/java/com/rulesClasses/Main.java
@@ -112,5 +112,109 @@ public class Main {
 
         // and then dispose the session
         ksession.dispose();
+        
+        
+        //TODO listas
+        public SymptomsPojo Shortness_breath = new SymptomsPojo(1,"shortness_breath", 0,a,b,listaD);
+    	public SymptomsPojo Rapid_Breathing = new SymptomsPojo(2,"rapid_breathing", 0,a,b,listaD);
+    	public SymptomsPojo Wheezing = new SymptomsPojo(3,"wheezing", 0,a,b,listaD);
+    	public SymptomsPojo Cyanosis = new SymptomsPojo(4,"cyanosis", 0,a,b,listaD);
+    	public SymptomsPojo Headache = new SymptomsPojo(5,"headache", 0,a,b,listaD);
+    	public SymptomsPojo Confusion_Disorientation = new SymptomsPojo(6,"confusion_disorientation", 0,a,b,listaD);
+    	public SymptomsPojo Irregular_heartbeat = new SymptomsPojo(7,"irregular_heartbeat", 0,a,b,listaD);
+    	public SymptomsPojo Chest_pain = new SymptomsPojo(8,"chest_pain", 0,a,b,listaD);
+    	public SymptomsPojo Fatigue_Weakness = new SymptomsPojo(9,"fatigue_weakness", 0,a,b,listaD);
+    	public SymptomsPojo Dizziness = new SymptomsPojo(10,"dizziness", 0,a,b,listaD);
+    	public SymptomsPojo Fainting_or_loss_of_consciousness = new SymptomsPojo(11,"fainting_or_loss_of_consciousness", 0,a,b,listaD);
+    	public SymptomsPojo Excessive_daytime_sleepiness = new SymptomsPojo(12,"excessive_daytime_sleepiness", 0,a,b,listaD);
+    	public SymptomsPojo Difficulty_concentrating = new SymptomsPojo(13,"difficulty_concentrating", 0,a,b,listaD);
+    	public SymptomsPojo Irritability = new SymptomsPojo(14,"irritability", 0,a,b,listaD);
+    	public SymptomsPojo Depression = new SymptomsPojo(15,"depression", 0,a,b,listaD);
+    	public SymptomsPojo Anxiety = new SymptomsPojo(16,"anxiety", 0,a,b,listaD);
+    	public SymptomsPojo Restless_sleep = new SymptomsPojo(17,"restless_sleep", 0,a,b,listaD);
+    	public SymptomsPojo Insomnia = new SymptomsPojo(18,"insomnia", 0,a,b,listaD);
+    	public SymptomsPojo Dry_mouth_or_sore_throat = new SymptomsPojo(19,"dry_mouth_or_sore_throat", 0,a,b,listaD);
+    	public SymptomsPojo listaDhest_oppression_discomfort = new SymptomsPojo(21,"listaDhest_oppression_discomfort", 0,a,b,listaD);
+    	public SymptomsPojo Muscle_or_body_pain = new SymptomsPojo(22,"muscle_or_body_pain", 0,a,b,listaD);
+    	public SymptomsPojo Cough = new SymptomsPojo(23,"cough", 0,a,b,listaD);
+    	public SymptomsPojo Cough_blood = new SymptomsPojo(24,"cough_blood", 0,a,b,listaD);
+    	public SymptomsPojo Cough_worse_over_time = new SymptomsPojo(25,"cough_worse_over_time", 0,a,b,listaD);
+    	public SymptomsPojo Difficulty_physical_activities = new SymptomsPojo(26,"difficulty_physical_activities", 0,a,b,listaD);
+    	public SymptomsPojo Loss_appetite_weight = new SymptomsPojo(27,"loss_appetite_weight", 0,a,b,listaD);
+    	public SymptomsPojo Hoarseness = new SymptomsPojo(28,"hoarseness", 0,a,b,listaD);
+    	public SymptomsPojo Trouble_swallowing = new SymptomsPojo(29,"trouble_swallowing", 0,a,b,listaD);
+    	public SymptomsPojo Fever_chills = new SymptomsPojo(30,"fever_chills", 0,a,b,listaD);
+    	public SymptomsPojo Loss_taste_smell = new SymptomsPojo(31,"loss_taste_smell", 0,a,b,listaD);
+    	public SymptomsPojo Congestion_or_running_nose = new SymptomsPojo(32,"congestion_or_running_nose", 0,a,b,listaD);
+    	public SymptomsPojo Nausea_vomiting = new SymptomsPojo(33,"nausea_vomiting", 0,a,b,listaD);
+    	public SymptomsPojo Inflammation_respiratory_tract = new SymptomsPojo(34,"inflammation_respiratory_tract", 0,a,b,listaD);
+    	public SymptomsPojo Sweating = new SymptomsPojo(35,"sweating", 0,a,b,listaD);
+    	public SymptomsPojo Mucus = new SymptomsPojo(36,"mucus", 0,a,b,listaD);
+    	public SymptomsPojo Expectoration = new SymptomsPojo(37,"expectoration", 0,a,b,listaD);
+    	public SymptomsPojo Stridor = new SymptomsPojo(38,"stridor", 0,a,b,listaD);
+    	public SymptomsPojo Tachycardia = new SymptomsPojo(39,"tachycardia", 0,a,b,listaD);
+    	public SymptomsPojo Hyperthermia = new SymptomsPojo(40,"hyperthermia", 0,a,b,listaD);
+    	public SymptomsPojo Loss_negative_pressure = new SymptomsPojo(41,"loss_negative_pressure", 0,a,b,listaD);
+    	public SymptomsPojo Sneezing = new SymptomsPojo(42,"sneezing", 0,a,b,listaD);
+    	public SymptomsPojo Chronic_cough = new SymptomsPojo(43,"chronic_cough", 0,a,b,listaD);
+
+    	
+    	
+    	public DiseasePojo Hypoxemia = new DiseasePojo(1,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Apnea = new DiseasePojo(2,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Hyperapnea = new DiseasePojo(3,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Tachypnea = new DiseasePojo(4,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Dysnea = new DiseasePojo(5,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Respiratory_Insufficiency = new DiseasePojo(6,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Lung_cancer = new DiseasePojo(7,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo COVID = new DiseasePojo(8,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Asthma = new DiseasePojo(9,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Emphysema = new DiseasePojo(10,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Bronchitis_Chronic = new DiseasePojo(11,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Atelactasis = new DiseasePojo(12,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Pneumothorax = new DiseasePojo(13,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Pneumonia = new DiseasePojo(14,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Malignant_Pleura_Effusion = new DiseasePojo(15,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Pulmonary_Embolism = new DiseasePojo(16,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Cold = new DiseasePojo(17,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+    	public DiseasePojo Flu = new DiseasePojo(18,"Hypoxemia","BI","Link",(float) 23,listaP,listaS);
+
+ 
+    	
+    	
+    	DrugPojo Betablockers = new DrugPojo("Betablockers");
+    	DrugPojo NSAIDs = new DrugPojo("NSAIDs");
+    	DrugPojo Opiods = new DrugPojo("Opiods");
+    	DrugPojo Statins = new DrugPojo("Statins");
+    	
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
